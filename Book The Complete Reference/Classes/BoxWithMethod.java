83a79e4a73a70ad51b3ea0401a336a92ecd32105
Dhruv Parmar <parmardhruv2615@gmail.com>
2024-08-17 21:00:31 +0530

    added same name as instance variable in parameters use of this to resolve namespace collisions

--- Book The Complete Reference/Classes/BoxWithMethod.java
+++ Book The Complete Reference/Classes/BoxWithMethod.java
@@ -9,10 +9,10 @@ class Box {
     double height;
     double depth;
 
-    Box(double w, double h, double d){
-        width = w;
-        height = h;
-        depth = d;
+    Box(double width, double height, double depth){
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
     }
 
     // Display volume
